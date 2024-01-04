f198ba9b61f9819690e644e66971ad6379e1f481
TendryMamitiana <Tendrywar@gmail.com>
2024-01-04 15:40:34 +0300

    finish 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    let result = "";
+    string = word.split("");
+    for(let i = string.length - 1; i >= 0; i--){
+        result += string[i];
+    }
+    return result;
 };
 
 // Do not edit below this line
