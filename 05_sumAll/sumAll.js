c2aa8961d92ec44fb5fad6d114f28b5b0e319933
TendryMamitiana <Tendrywar@gmail.com>
2024-01-04 16:49:11 +0300

    finish 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,26 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    
+   if(num1 < 0 || num2 < 0 || typeof num1 !== "number" || typeof num2 !== "number" ){
+     return "ERROR";
+   } else {
+        let result = 0;
+        let difference = Math.abs(num1 - num2); 
+   
+        let max = 0;
+        if(num1 > num2){
+            max = num1;
+        } else if(num2 > num1){
+            max = num2;
+        }
+        for(let i = 0; i <= difference; i++){    
+            result += max;
+            max--;
+        }
+        return result;
+   }
+   
 };
+
 
 // Do not edit below this line
 module.exports = sumAll;
