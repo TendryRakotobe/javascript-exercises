3db5af829b5070c13f05ae66a2cb981eaba179af
TendryMamitiana <Tendrywar@gmail.com>
2024-01-04 15:57:48 +0300

    finish 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ... elements) {
+    for (const element of elements){
+        for(let i = 0; i < array.length; i++){
+            if(element === array[i]){
+                array.splice(i, 1);
+            }
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
