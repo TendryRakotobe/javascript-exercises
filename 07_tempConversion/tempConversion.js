5bb7aa46ad3d4c4881040ee81250189912f25e82
TendryMamitiana <Tendrywar@gmail.com>
2024-01-05 13:27:46 +0300

    finish 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let result = (temp - 32) * (5/9);
+  result *= 10;
+  result = Math.round(result);
+  result /= 10;
+  return result;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let result = (temp * (9/5)) + 32;
+  result *= 10;
+  result = Math.round(result);
+  result /= 10;
+  return result;
 };
 
 // Do not edit below this line
