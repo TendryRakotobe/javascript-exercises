1376f5eb0f777c84473432744bb5d0806bf4be18
TendryMamitiana <Tendrywar@gmail.com>
2024-02-12 17:39:32 +0300

    finish calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,38 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce( (sum, item) => sum += item, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((multiply, value) => multiply * value);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  let arr = [];
+	if(a === 0) {
+    return 1;
+  } else {
+      for(let i = a; i > 0; i--){
+      arr.push(i);
+    }
+    console.log(arr);
+    return arr.reduce((result, value) => result * value);
+  }
+  
 };
+
+console.log(factorial(3));
 
 // Do not edit below this line
 module.exports = {
