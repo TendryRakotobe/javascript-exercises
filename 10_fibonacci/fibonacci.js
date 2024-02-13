75da51719a86507f9d69cd32eb7b528b4e255650
TendryMamitiana <Tendrywar@gmail.com>
2024-02-13 10:24:23 +0300

    finish fibonnacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+    number = parseInt(number);
+    if(number === 0) return 0
+    else if(number < 0) return "OOPS";
+    let ancien = 1;
+    let nouveau = 1;
+    for(let i = 2; i < number; i++){
+        let temp = nouveau;
+        nouveau += ancien;
+        ancien = temp;
+    }
+    return nouveau;
 };
 
 // Do not edit below this line
