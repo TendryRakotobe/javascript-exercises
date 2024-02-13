f619b9af605d03330e13d93bb56c14cf6f91c2f6
TendryMamitiana <Tendrywar@gmail.com>
2024-02-13 10:00:14 +0300

    finish palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,14 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    str = str
+        .split(' ').join('')
+        .split(',').join('').toLowerCase();
+    const str2 = str.split('').reverse().join('');
+    let str3 = str2.split('').slice();
+    const moveElement = str3.splice(0, 1);
+    str3.push(moveElement);
+    str3 = str3.join('');
+    return ((str === str2) || (str === str3));
 };
-
+console.log(palindromes('racecar!'));
 // Do not edit below this line
 module.exports = palindromes;
