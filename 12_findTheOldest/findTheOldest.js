ffbf1a616b085702b547e40278a7a08a463546ab
TendryMamitiana <Tendrywar@gmail.com>
2024-02-13 15:07:15 +0300

    delete all console.log on findTheOldest.js

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,19 +1,3 @@
-const people = [
-    {
-      name: "Carly",
-      yearOfBirth: 2011,
-    },
-    {
-      name: "Ray",
-      yearOfBirth: 1962,
-      yearOfDeath: 2011,
-    },
-    {
-      name: "Jane",
-      yearOfBirth: 1912,
-      yearOfDeath: 1941,
-    },
-  ]
 
 const findTheOldest = function(arr) {
     arr = arr.sort((a, b) => {
@@ -30,8 +14,6 @@ const findTheOldest = function(arr) {
     );
     return arr.shift();
 };
-
-console.log(findTheOldest(people));
 
 // Do not edit below this line
 module.exports = findTheOldest;
